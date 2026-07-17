c4f6db60e5357bf41639ae198283b19f5a0cc637
HillsideLab <121755027+HillsideLab@users.noreply.github.com>
2026-07-18 06:15:11 +0900

    create Zod Validation & Product Type Interface

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,22 +1,22 @@
 import ProductCard from "./product-card";
+import { Product } from "@/types";
 
 const ProdultList = ({
   data,
   title,
   limit,
 }: {
-  data: any;
+  data: Product[];
   title?: string;
   limit?: number;
 }) => {
-  console.log(data);
   const limitedData = limit ? data.slice(0, limit) : data;
   return (
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: any) => (
+          {limitedData.map((product: Product) => (
             <ProductCard key={product.slug} product={product} />
           ))}
         </div>
