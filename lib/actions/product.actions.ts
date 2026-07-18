a3320803aafd0f75407a6e4be1aa914d8caa1a3b
HillsideLab <121755027+HillsideLab@users.noreply.github.com>
2026-07-19 03:06:47 +0900

    create Prodcut Details Page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,7 +3,9 @@ import { PrismaClient } from '@/lib/generated/prisma/client';
 import { PrismaPg } from "@prisma/adapter-pg";
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { prisma } from '@/db/prisma';
 
+// Get latest products
 export async function getLatestProducts(){
        const adapter = new PrismaPg({
            connectionString: process.env.DATABASE_URL!,
@@ -19,5 +21,12 @@ export async function getLatestProducts(){
         });
 
         return convertToPlainObject(data);
+}
 
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string){
+    return await prisma.product.findFirst(
+        {where:{slug: slug}},
+    )
 }
