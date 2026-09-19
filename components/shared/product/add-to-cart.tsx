67b0ad5a8afdcf510ca1a435f921da818dc9ee28
hillsideLab <u703.fm@gmail.com>
2026-09-19 10:27:54 +0900

    create Dynamic Cart Button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,12 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { CartItem } from "@/types";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { Plus, Minus } from "lucide-react";
+import { Cart, CartItem } from "@/types";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { toast } from "@/components/ui/toast";
 
-const AddToCart = ({ item }:{ item: CartItem }) => {
+const AddToCart = ({ cart, item }:{ cart?: Cart, item: CartItem }) => {
     const router = useRouter();
     const handleAddToCart = async () =>{
         const res = await addItemToCart(item);
@@ -43,13 +43,39 @@ const AddToCart = ({ item }:{ item: CartItem }) => {
             },
         })
     }
-    return (
+
+    // Handle remove from cart
+    const handleRemoveFromCart = async () => {
+        const res = await removeItemFromCart(item.productId);
+
+        toast.add({
+            type: res.success ? 'success' : 'error',
+            description: res.message,
+        });
+
+        return;
+    };
+
+    // Check if item in cart
+    const existItem = cart && cart.items.find((x)=> x.productId === item.productId);
+
+    return  existItem? (
+        <div>
+            <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+                <Minus className="h-4 w-4"/>
+            </Button>
+            <span className="px-2">{existItem.qty}</span>
+            <Button type="button" variant="outline" onClick={handleAddToCart}>
+                <Plus className="h-4 w-4"/>
+            </Button>
+        </div>
+    ):(
         <Button
             className="w-full"
             type="button"
             onClick={handleAddToCart}
         >
-            Add To Cart
+            <Plus/> Add To Cart
         </Button>
     );
 }
