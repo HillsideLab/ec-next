e4a93219a01bc1be72ff39da174c6df60969fb4f
hillsideLab <u703.fm@gmail.com>
2026-09-19 15:05:16 +0900

    create shadCN UI Table

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -7,6 +7,8 @@ import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import {Cart} from "@/types";
 import Link from "next/link";
 import Image from "next/image"
+import {Table, TableBody, TableHead, TableHeader, TableRow, TableCell} from "@/components/ui/table"
+import { Button } from "@/components/ui/button";
 
 const CartTable = ({cart}: {cart?: Cart}) => {
     const router = useRouter();
@@ -19,7 +21,61 @@ const CartTable = ({cart}: {cart?: Cart}) => {
         </div>
     ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-            <div className="overflaw-x-auto md:col-span-3">Table</div>
+            <div className="overflow-x-auto md:col-span-3">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Item</TableHead>
+                            <TableHead className="text-center">Quantity</TableHead>
+                            <TableHead className="text-right">Price</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {cart.items.map((item)=>(
+                            <TableRow key={item.slug}>
+                                <TableCell>
+                                    <Link href={`/product/${item.slug}`} className="flex items-center">
+                                    <Image src={item.image} alt={item.name} width={50} height={50}/>
+                                    <span className="px-2">{item.name}</span>
+                                    </Link>
+                                </TableCell>
+                                <TableCell>
+                                    <div className="flex items-center justify-center gap-2">
+                                        <Button disabled={isPending} variant="outline" type="button" onClick={()=> startTransition(async()=>{
+                                            const res = await removeItemFromCart(item.productId);
+                                            toast.add({
+                                                type: res.success ? 'success' : 'error',
+                                                description: res.message,
+                                            });
+                                        })}>
+                                        { isPending ? (
+                                            <Loader className="w-4 h-4 animate-spin" />
+                                        ):(
+                                            <Minus className="w-4 h-4"/>
+                                        )}
+                                        </Button>
+                                        <span>{item.qty}</span>
+                                        <Button disabled={isPending} variant="outline" type="button" onClick={()=> startTransition(async()=>{
+                                            const res = await addItemToCart(item);
+                                            toast.add({
+                                                type: res.success ? 'success' : 'error',
+                                                description: res.message,
+                                            });
+                                        })}>
+                                        { isPending ? (
+                                            <Loader className="w-4 h-4 animate-spin" />
+                                        ):(
+                                            <Plus className="w-4 h-4"/>
+                                        )}
+                                        </Button>
+                                    </div>
+                                </TableCell>
+                                <TableCell className="text-right">${item.price}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     )}
     </>;
